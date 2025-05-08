8f1552fce90a587ac5217fc34db8c30a2b58a17b
oscarale <mail@aleyoscar.com>
2025-05-08 00:17:25 -0500

    fix(web): Date showing as local time instead of UTC

--- static/main.js
+++ static/main.js
@@ -38,9 +38,9 @@ async function fetchTasks() {
 
 // Get date in YYYY-MM-DD format
 function getDateString(date) {
-	const yyyy = date.getFullYear();
-	const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so +1
-	const dd = String(date.getDate()).padStart(2, '0');
+	const yyyy = date.getUTCFullYear();
+	const mm = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are 0-based, so +1
+	const dd = String(date.getUTCDate()).padStart(2, '0');
 	return `${yyyy}-${mm}-${dd}`;
 }
 
