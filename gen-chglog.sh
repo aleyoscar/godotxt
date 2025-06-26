48455f104240965596b5bb2e413c0d3885cb12b6
oscarale <mail@aleyoscar.com>
2025-06-26 10:21:56 -0500

    feat(core): Update version info with new tag

--- gen-chglog.sh
+++ gen-chglog.sh
@@ -7,6 +7,11 @@ else
 	TAG_CONFIG=.chglog/config-tag.yml
 	TAG_FILE=.chglog/current-tag.md
 
+	echo "Updating version info in README.md and app.py to $1"
+	# sed -i "1s/.*/const version = '$1';/" sw.js
+	sed -i "10s/.*/VERSION = '$1'/" app.py
+	sed -i "3s/.*/> $1/" README.md
+
 	echo "Generating CHANGELOG file"
 	git-chglog --next-tag $1 -o $LOG_FILE
 
